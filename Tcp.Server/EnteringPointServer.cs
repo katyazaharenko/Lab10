f70b83a5e6771015acc0726b48e3de425bd52b5f
Ekaterina Zakharenko <45923491+katyazaharenko@users.noreply.github.com>
2020-05-18 11:20:29 +0300

    Add files via upload

---
+++ Tcp.Server/EnteringPointServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SomeProject.Library.Server;
+using SomeProject.Library;
+using SomeProject;
+
+namespace SomeProject.TcpServer
+{
+    public class EnteringPointServer
+    {
+        public static void Main(string[] args)
+        {
+           try
+            {
+                Server server = new Server();
+                // Включение сервера.
+                server.TurnOnListener().Wait();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
